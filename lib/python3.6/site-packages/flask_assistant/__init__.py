2981529d44c81d9935707a55c0d71915b71c4576
入江嵐士郎 <ranshiroirie@gmail.com>
2018-08-06 14:53:06 +0900

    first commit

---
+++ lib/python3.6/site-packages/flask_assistant/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+logger = logging.getLogger('flask_assistant')
+logger.addHandler(logging.StreamHandler())
+if logger.level == logging.NOTSET:
+    logger.setLevel(logging.INFO)
+
+
+from flask_assistant.core import (
+    Assistant,
+    context_manager,
+    intent,
+    request,
+    access_token
+)
+
+from flask_assistant.response import ask, tell, event, build_item, permission
+from flask_assistant.manager import Context
+
+import flask_assistant.utils
+
+from api_ai.api import ApiAi
